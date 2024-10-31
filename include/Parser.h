be24b871545402f2e45908156cfdc0737135ed92
Diego Lorenzo Villalobos <diegolorenzovillalobos@gmail.com>
2024-10-31 23:40:22 +0000

    added all functions

--- include/Parser.h
+++ include/Parser.h
@@ -13,7 +13,7 @@ public:
         currentToken = tokens.front();
     }
 
-    std::unique_ptr<ASTNode> parseProgram();
+    std::unique_ptr<ASTNode> parsePROGRAM(); // this is the starting point
 
 private:
     TokenTable &tokens;
@@ -23,14 +23,106 @@ private:
     void expect(TokenType expected); // consume el token si es del tipo que queremos
 
     std::unique_ptr<ASTNode> parseGlobalDeclarations();
-    std::unique_ptr<ListNode> parseProgramPrime();
 
-    std::unique_ptr<ASTNode> parseFunctionDefinition();
-    std::unique_ptr<ASTNode> parseDeclaration();
-
-    std::unique_ptr<ASTNode> parseStatement();
-    std::unique_ptr<ASTNode> parseExpression();
+    std::unique_ptr<ASTNode> parseADDITIVE_EXPRESSION();
+    std::unique_ptr<ASTNode> parseADDITIVE_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseAND_EXPRESSION();
+    std::unique_ptr<ASTNode> parseAND_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseARGUMENT_EXPRESSION_LIST();
+    std::unique_ptr<ASTNode> parseARGUMENT_EXPRESSION_LIST_PRIME();
+    std::unique_ptr<ASTNode> parseASSIGNMENT_EXPRESSION();
+    std::unique_ptr<ASTNode> parseASSIGNMENT_EXPRESSION_FAC();
+    std::unique_ptr<ASTNode> parseASSIGNMENT_EXPRESSION_OPT_FAC();
+    std::unique_ptr<ASTNode> parseASSIGNMENT_OPERATOR();
+    std::unique_ptr<ASTNode> parseBLOCK();
+    std::unique_ptr<ASTNode> parseBLOCK_CONTENT();
+    std::unique_ptr<ASTNode> parseBLOCK_ITEM();
+    std::unique_ptr<ASTNode> parseBLOCK_ITEM_LIST();
+    std::unique_ptr<ASTNode> parseBLOCK_ITEM_LIST_PRIME();
+    std::unique_ptr<ASTNode> parseCONDITIONAL_EXPRESSION();
+    std::unique_ptr<ASTNode> parseCONDITIONAL_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseCONSTANT();
+    std::unique_ptr<ASTNode> parseDECLARATION();
+    std::unique_ptr<ASTNode> parseDECLARATION_FAC();
+    std::unique_ptr<ASTNode> parseDECLARATION_LIST();
+    std::unique_ptr<ASTNode> parseDECLARATION_LIST_PRIME();
+    std::unique_ptr<ASTNode> parseDECLARATION_SPECIFIERS();
+    std::unique_ptr<ASTNode> parseDECLARATION_SPECIFIERS_PRIME();
+    std::unique_ptr<ASTNode> parseDECLARATOR();
+    std::unique_ptr<ASTNode> parseDIRECT_DECLARATOR();
+    std::unique_ptr<ASTNode> parseDIRECT_DECLARATOR_PRIME();
+    std::unique_ptr<ASTNode> parseEQUALITY_EXPRESSION();
+    std::unique_ptr<ASTNode> parseEQUALITY_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseEXCLUSIVE_OR_EXPRESSION();
+    std::unique_ptr<ASTNode> parseEXCLUSIVE_OR_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseEXPRESSION();
+    std::unique_ptr<ASTNode> parseEXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseEXPRESSION_STATEMENT();
+    std::unique_ptr<ASTNode> parseFOR_INIT_STATEMENT();
+    std::unique_ptr<ASTNode> parseFOR_OPTIONAL_EXPRESSION();
+    std::unique_ptr<ASTNode> parseFUNCTION_DEFINITION();
+    std::unique_ptr<ASTNode> parseFUNCTION_DEF_FAC();
+    std::unique_ptr<ASTNode> parseFUNCTION_SPECIFIER();
+    std::unique_ptr<ASTNode> parseGLOBAL_DECLARATIONS();
+    std::unique_ptr<ASTNode> parseIDENTIFIER_LIST();
+    std::unique_ptr<ASTNode> parseIDENTIFIER_LIST_PRIME();
+    std::unique_ptr<ASTNode> parseINCLUSIVE_OR_EXPRESSION();
+    std::unique_ptr<ASTNode> parseINCLUSIVE_OR_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseINITIALIZER();
+    std::unique_ptr<ASTNode> parseINITIALIZER_BRACE_FAC();
+    std::unique_ptr<ASTNode> parseINITIALIZER_LIST();
+    std::unique_ptr<ASTNode> parseINITIALIZER_LIST_PRIME();
+    std::unique_ptr<ASTNode> parseINIT_DECLARATOR();
+    std::unique_ptr<ASTNode> parseINIT_DECLARATOR_FAC();
+    std::unique_ptr<ASTNode> parseINIT_DECLARATOR_LIST();
+    std::unique_ptr<ASTNode> parseINIT_DECLARATOR_LIST_PRIME();
+    std::unique_ptr<ASTNode> parseITERATION_STATEMENT();
+    std::unique_ptr<ASTNode> parseJUMP_STATEMENT();
+    std::unique_ptr<ASTNode> parseLABELED_STATEMENT();
+    std::unique_ptr<ASTNode> parseLOGICAL_AND_EXPRESSION();
+    std::unique_ptr<ASTNode> parseLOGICAL_AND_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseLOGICAL_OR_EXPRESSION();
+    std::unique_ptr<ASTNode> parseLOGICAL_OR_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseMULTIPLICATIVE_EXPRESSION();
+    std::unique_ptr<ASTNode> parseMULTIPLICATIVE_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parsePARAMETER_DECLARATION();
+    std::unique_ptr<ASTNode> parsePARAMETER_LIST();
+    std::unique_ptr<ASTNode> parsePARAMETER_LIST_OPT_FAC();
+    std::unique_ptr<ASTNode> parsePARAMETER_LIST_PRIME();
+    std::unique_ptr<ASTNode> parsePARAM_DECL_FAC();
+    std::unique_ptr<ASTNode> parsePOINTER();
+    std::unique_ptr<ASTNode> parsePOSTFIX_ARGUMENTS();
+    std::unique_ptr<ASTNode> parsePOSTFIX_EXPRESSION();
+    std::unique_ptr<ASTNode> parsePOSTFIX_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parsePRIMARY_EXPRESSION();
+    std::unique_ptr<ListNode> parsePROGRAM_PRIME();
+    std::unique_ptr<ASTNode> parseRELATIONAL_EXPRESSION();
+    std::unique_ptr<ASTNode> parseRELATIONAL_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseSELECTION_STATEMENT();
+    std::unique_ptr<ASTNode> parseSHIFT_EXPRESSION();
+    std::unique_ptr<ASTNode> parseSHIFT_EXPRESSION_PRIME();
+    std::unique_ptr<ASTNode> parseSPECIFIER_QUALIFIER_LIST();
+    std::unique_ptr<ASTNode> parseSTATEMENT();
+    std::unique_ptr<ASTNode> parseSTATIC_OPT_FAC();
+    std::unique_ptr<ASTNode> parseSTORAGE_SPECIFIER();
+    std::unique_ptr<ASTNode> parseSTRUCT_DECLARATION();
+    std::unique_ptr<ASTNode> parseSTRUCT_DECLARATION_LIST();
+    std::unique_ptr<ASTNode> parseSTRUCT_DECLARATION_LIST_PRIME();
+    std::unique_ptr<ASTNode> parseSTRUCT_DECLARATOR_LIST();
+    std::unique_ptr<ASTNode> parseSTRUCT_DECLARATOR_LIST_PRIME();
+    std::unique_ptr<ASTNode> parseSTRUCT_DECL_FAC();
+    std::unique_ptr<ASTNode> parseSTRUCT_SPECIFIER();
+    std::unique_ptr<ASTNode> parseSTRUCT_SPEC_FAC();
+    std::unique_ptr<ASTNode> parseSTRUCT_SPEC_FAC2();
+    std::unique_ptr<ASTNode> parseTYPE_QUALIFIER();
+    std::unique_ptr<ASTNode> parseTYPE_QUALIFIER_LIST_OPT_FAC();
+    std::unique_ptr<ASTNode> parseTYPE_SPECIFIER();
+    std::unique_ptr<ASTNode> parseUNARY_EXPRESSION();
 
     bool isFirstTokenForProgramPrime();
     bool isBasicType();
 };
+
+/*
+
+*/
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -2,80 +2,9 @@
 #include "Parser.h"
 #include <unordered_set>
 
-void Parser::consume()
-{
-    tokens.pop_front();
-    this->currentToken = tokens.front();
-}
-
-void Parser::expect(TokenType expected)
-{
-    if (currentToken.getType() != expected)
-    {
-        throw std::runtime_error("Unexpected token: " + currentToken.getValue());
-    }
-    consume();
-}
-
-std::unique_ptr<ASTNode> Parser::parseProgram()
-{
-    auto programNode = std::make_unique<ProgramNode>();
-    programNode->push_back_node(parseGlobalDeclarations());
-    programNode->push_back_vec(parseProgramPrime());
-
-    if (currentToken.getType() != TokenType::END_OF_FILE)
-    {
-        throw std::runtime_error("Unexpected token: " + currentToken.getValue());
-    }
-    return programNode;
-}
-
-// PROGRAM_PRIME -> GLOBAL_DECLARATIONS PROGRAM_PRIME | ϵ
-std::unique_ptr<ListNode> Parser::parseProgramPrime()
-{
-    auto programPrimeNode = std::make_unique<GlobalDeclarationsNode>();
-
-    // Check if the current token is the first token for PROGRAM_PRIME
-    while (isFirstTokenForProgramPrime()) // Continue as long as there are valid tokens
-    {
-        // Parse another set of global declarations
-        auto additionalDeclarations = parseGlobalDeclarations();
-        programPrimeNode->push_back_node(std::move(additionalDeclarations));
-    }
-
-    return programPrimeNode;
-}
-
-// GLOBAL_DECLARATIONS -> DECLARATION | FUNCTION_DEFINITION
-std::unique_ptr<ASTNode> Parser::parseGlobalDeclarations()
-{
-    if (currentToken.getType() == TokenType::KEYWORD && currentToken.getValue() == "func")
-    {
-        auto declarationNode = std::make_unique<DeclarationNode>();
-        declarationNode->setDeclaration(parseFunctionDefinition());
-        return declarationNode;
-    }
-
-    if (isBasicType())
-    {
-        auto declarationNode = std::make_unique<DeclarationNode>();
-        declarationNode->setDeclaration(parseDeclaration());
-        return declarationNode;
-    }
-
-    return nullptr;
-}
-
-std::unique_ptr<ASTNode> Parser::parseFunctionDefinition()
-{
-    return nullptr;
-}
-
-std::unique_ptr<ASTNode> Parser::parseDeclaration()
-{
-    return nullptr;
-}
-// helper functions:
+/*
+HELPER FUNCTIONS
+*/
 bool Parser::isBasicType()
 {
     std::string tokenValue = currentToken.getValue();
@@ -100,3 +29,165 @@ bool Parser::isFirstTokenForProgramPrime()
     // Check if the current token's value is in the set of valid keywords
     return validKeywords.find(currentToken.getValue()) != validKeywords.end();
 }
+
+void Parser::consume()
+{
+    tokens.pop_front();
+    this->currentToken = tokens.front();
+}
+
+void Parser::expect(TokenType expected)
+{
+    if (currentToken.getType() != expected)
+    {
+        throw std::runtime_error("Unexpected token: " + currentToken.getValue());
+    }
+    consume();
+}
+
+/*
+PARSER
+*/
+
+std::unique_ptr<ASTNode> Parser::parsePROGRAM()
+{
+    auto programNode = std::make_unique<ProgramNode>();
+    programNode->push_back_node(parseGLOBAL_DECLARATIONS());
+    programNode->push_back_vec(parsePROGRAM_PRIME());
+
+    if (currentToken.getType() != TokenType::END_OF_FILE)
+    {
+        throw std::runtime_error("Unexpected token: " + currentToken.getValue());
+    }
+    return programNode;
+}
+
+// PROGRAM_PRIME -> GLOBAL_DECLARATIONS PROGRAM_PRIME | ϵ
+std::unique_ptr<ListNode> Parser::parsePROGRAM_PRIME()
+{
+    auto programPrimeNode = std::make_unique<GlobalDeclarationsNode>();
+
+    // Check if the current token is the first token for PROGRAM_PRIME
+    while (isFirstTokenForProgramPrime()) // Continue as long as there are valid tokens
+    {
+        // Parse another set of global declarations
+        auto additionalDeclarations = parseGlobalDeclarations();
+        programPrimeNode->push_back_node(std::move(additionalDeclarations));
+    }
+
+    return programPrimeNode;
+}
+
+// GLOBAL_DECLARATIONS -> DECLARATION | FUNCTION_DEFINITION
+std::unique_ptr<ASTNode> Parser::parseGLOBAL_DECLARATIONS()
+{
+    if (currentToken.getType() == TokenType::KEYWORD && currentToken.getValue() == "func")
+    {
+        auto declarationNode = std::make_unique<DeclarationNode>();
+        declarationNode->setDeclaration(parseFUNCTION_DEFINITION());
+        return declarationNode;
+    }
+
+    if (isBasicType())
+    {
+        auto declarationNode = std::make_unique<DeclarationNode>();
+        declarationNode->setDeclaration(parseDECLARATION());
+        return declarationNode;
+    }
+
+    return nullptr;
+}
+
+std::unique_ptr<ASTNode> Parser::parseADDITIVE_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseADDITIVE_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseAND_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseAND_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseARGUMENT_EXPRESSION_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseARGUMENT_EXPRESSION_LIST_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseASSIGNMENT_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseASSIGNMENT_EXPRESSION_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseASSIGNMENT_EXPRESSION_OPT_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseASSIGNMENT_OPERATOR() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseBLOCK() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseBLOCK_CONTENT() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseBLOCK_ITEM() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseBLOCK_ITEM_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseBLOCK_ITEM_LIST_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseCONDITIONAL_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseCONDITIONAL_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseCONSTANT() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseDECLARATION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseDECLARATION_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseDECLARATION_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseDECLARATION_LIST_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseDECLARATION_SPECIFIERS() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseDECLARATION_SPECIFIERS_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseDECLARATOR() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseDIRECT_DECLARATOR() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseDIRECT_DECLARATOR_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseEQUALITY_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseEQUALITY_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseEXCLUSIVE_OR_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseEXCLUSIVE_OR_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseEXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseEXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseEXPRESSION_STATEMENT() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseFOR_INIT_STATEMENT() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseFOR_OPTIONAL_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseFUNCTION_DEFINITION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseFUNCTION_DEF_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseFUNCTION_SPECIFIER() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseGLOBAL_DECLARATIONS() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseIDENTIFIER_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseIDENTIFIER_LIST_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINCLUSIVE_OR_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINCLUSIVE_OR_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINITIALIZER() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINITIALIZER_BRACE_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINITIALIZER_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINITIALIZER_LIST_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINIT_DECLARATOR() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINIT_DECLARATOR_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINIT_DECLARATOR_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseINIT_DECLARATOR_LIST_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseITERATION_STATEMENT() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseJUMP_STATEMENT() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseLABELED_STATEMENT() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseLOGICAL_AND_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseLOGICAL_AND_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseLOGICAL_OR_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseLOGICAL_OR_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseMULTIPLICATIVE_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseMULTIPLICATIVE_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePARAMETER_DECLARATION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePARAMETER_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePARAMETER_LIST_OPT_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePARAMETER_LIST_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePARAM_DECL_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePOINTER() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePOSTFIX_ARGUMENTS() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePOSTFIX_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePOSTFIX_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parsePRIMARY_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseRELATIONAL_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseRELATIONAL_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSELECTION_STATEMENT() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSHIFT_EXPRESSION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSHIFT_EXPRESSION_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSPECIFIER_QUALIFIER_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTATEMENT() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTATIC_OPT_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTORAGE_SPECIFIER() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTRUCT_DECLARATION() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTRUCT_DECLARATION_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTRUCT_DECLARATION_LIST_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTRUCT_DECLARATOR_LIST() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTRUCT_DECLARATOR_LIST_PRIME() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTRUCT_DECL_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTRUCT_SPECIFIER() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTRUCT_SPEC_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseSTRUCT_SPEC_FAC2() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseTYPE_QUALIFIER() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseTYPE_QUALIFIER_LIST_OPT_FAC() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseTYPE_SPECIFIER() { return nullptr; }
+std::unique_ptr<ASTNode> Parser::parseUNARY_EXPRESSION() { return nullptr; }
