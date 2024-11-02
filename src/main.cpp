704f6986f811e35a6058e1038ebba8e1fd1095c8
Diego Lorenzo Villalobos <diegolorenzovillalobos@gmail.com>
2024-11-02 01:52:36 +0000

    catch exeption in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,19 @@
 
 int main() {
     Compiler compiler("./test.cmm", 0);
-    int res = compiler.compile();
-    if (res == 0) {
-        std::cout << std::endl << "Compilation successful." << std::endl;
-    } else {
+    try {
+        int res = compiler.compile();
+        // Throwing an exception with a custom message
+        if (res == 0) {
+            std::cout << std::endl << "Compilation successful." << std::endl;
+        } else {
+            std::cout << std::endl << "Compilation failed." << std::endl;
+        }
+    } catch (const std::exception& e) {
+        // Catching the exception and retrieving the message
+        std::cout << "Caught exception: " << e.what() << std::endl;
         std::cout << std::endl << "Compilation failed." << std::endl;
     }
+
     return 0;
 }
